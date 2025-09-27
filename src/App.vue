8c2d774ae40e6dd6ecb3791e09658f1575035a80
Antônio Carlos <antonioocarlos@proton.me>
2025-09-27 13:49:25 -0300

    fix: fix alignment issues

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,21 @@
 </script>
 
 <template>
-  <div class="bg-black flex flex-col w-full h-screen">
+  <div class="bg-black flex flex-col w-full h-[97.5vh]">
     <header class="flex items-center justify-center m-4 h-10">
-      <img src="./assets/logo.png" />
+      <img class="cursor-pointer"src="./assets/logo.png" />
 
       <div class="flex-1"></div>
 
-      <div class="flex items-center px-25 py-2 justify-center bg-[#3c3c3c] rounded-md">
+      <div class="flex items-center px-25 py-2 justify-center bg-[#3c3c3c] rounded-md cursor-pointer">
         <img class="mr-3 w-4" src="./assets/search.png" />
         <span class="text-white font-[Fredoka]">Search</span>
       </div>
 
       <div class="flex-1"></div>
 
-      <div class="flex items-center justify-center">
-        <img class="mr-4 w-[25px]" src="./assets/config.png" />
+      <div class="flex items-center justify-center cursor-pointer">
+        <img class="mr-2 w-[25px]" src="./assets/config.png" />
         <div class="bg-[#85E014] size-10 text-lg rounded-full text-white font-bold flex items-center justify-center">A</div>
       </div>
     </header>
@@ -33,14 +33,14 @@
       <div class="box-border flex-1  flex">
 
         <div class="w-1/3 bg-red-500 h-full m-2 rounded-md mt-0 ">
-          <div class=""><img class="mr-3 w-4" src="./assets/home.png"></div>
+          <button class="box-border bg-[#3c3c3c]"><img class="mr-3 w-4" src="./assets/home.png"></button>
           
         </div>
 
         <div class="w-full bg-blue-500 h-full m-2 rounded-md mt-0"></div>
       </div>
       
-      <div class="w-full h-10 bg-green-500 m-2 rounded-md "></div>
+      <div class="w-full h-10  bg-green-500 m-2 rounded-md  "></div>
 
   </div>
 </template>
