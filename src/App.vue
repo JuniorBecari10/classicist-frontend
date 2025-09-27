b2aa642604d2973f826de27471efb88de71e7d2e
Antônio Carlos <antonioocarlos@proton.me>
2025-09-27 13:46:40 -0300

    feat: reinit frontend

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
-<script setup lang="ts">
-    import { ref } from "vue";
-
-    let isHello = ref(true);
-    const flip = () => isHello.value ^= true;
-</script>
-
 <template>
-    <button class="
-        bg-sky-500 hover:bg-white
-        text-white hover:text-black
-        rounded-full py-2 px-4 m-4 cursor-pointer
-        duration-200 ease-in-out"
-        @click="flip">
-        Change
-    </button>
+    <div class="shadow-md bg-red"><p>musica</p>
 
-    <h1 class="text-3xl font-fredoka font-bold"
-        v-if="isHello">Hello</h1>
-    <h1 class="text-3xl font-fredoka font-bold"
-        v-else>World</h1>
+    </div>
+
 </template>
