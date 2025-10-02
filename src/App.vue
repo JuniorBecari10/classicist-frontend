ea092a56e703623cf7582e3afb0b39268a882529
druma <80527920+Drumae@users.noreply.github.com>
2025-10-02 00:10:45 +0000

    feat: initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,8 @@
 <script setup lang="ts">
-    import { ref } from "vue";
-
-    let isHello = ref(true);
-    const flip = () => isHello.value ^= true;
+import { ref } from "vue";
+import Comp from "./Comp.vue";
 </script>
 
 <template>
-    <button class="
-        bg-sky-500 hover:bg-white
-        text-white hover:text-black
-        rounded-full py-2 px-4 m-4 cursor-pointer
-        duration-200 ease-in-out"
-        @click="flip">
-        Change
-    </button>
-
-    <h1 class="text-3xl font-fredoka font-bold"
-        v-if="isHello">Hello</h1>
-    <h1 class="text-3xl font-fredoka font-bold"
-        v-else>World</h1>
+    <Comp></Comp>
 </template>
