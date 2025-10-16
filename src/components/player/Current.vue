ce156cbf56cf73763e4a2fa6b10e68276f3bb38f
Antônio Carlos <antonioocarlos@proton.me>
2025-10-15 21:33:55 -0300

    feat (player): add progress bar

--- src/components/player/Current.vue
+++ src/components/player/Current.vue
@@ -14,15 +14,15 @@
 </script>
 
 <template>
-    <div class="h-full flex items-center ml-5">
+    <div class="h-full w-1/4 flex items-center ml-5">
         <img src="https://github.com/JuniorBecari10/classicist-public/blob/main/files/images/covers/symp5.jpg?raw=true"
             class="size-15 mr-5 rounded-lg">
 
-        <div class="flex flex-col justify-center items-start w-1/6 truncate">
+        <div class="flex flex-col justify-center items-start w-full truncate">
             <span class="font-fredoka font-semibold text-lg" :title="name">{{ name }}</span>
             <span class="font-fredoka text-fgray text-sm" :title="authors">{{ authors }}</span>
         </div>
 
-        <div class="relative right-12 size-12 bg-linear-to-r from-transparent to-fg"></div>
+        <div class="relative right-8 size-12 bg-linear-to-r from-transparent to-fg"></div>
     </div>
 </template>
