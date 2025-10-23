7a511b8e09dc04cd423df0a9f586a246e3ce011d
druma <80527920+Drumae@users.noreply.github.com>
2025-10-23 23:24:06 +0000

    feat: added reactivity

--- src/components/Composer.vue
+++ src/components/Composer.vue
@@ -4,9 +4,9 @@ import About from './About.vue'
 </script>
 
 <template>
-    <section class="font-fredoka">
-        <div class="bg-[url('./src/resources/Amadeus.jpg')] h-60 bg-[center_-40rem]">
-            <div class="h-1/1 bg-gradient-to-t from-[#252525] to-transparent to-80%">
+    <section class="font-fredoka select-none">
+        <div class="bg-[url('./src/resources/Amadeus.jpg')] h-60 bg-[center_-40rem] bg-fixed">
+            <div class="h-1/1 bg-gradient-to-t from-[#252525] to-transparent">
                 <div class="pt-5 pl-8 pt-0">
                     <p class="font-semibold text-[3rem]">Wolfgang Amadeus Mozart</p>
                     <p class="font-light text-[1.5rem] text-gray-300">Composer • 1756 ― 1791</p>
