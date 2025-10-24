fea2b976385286eee34ecb983903ee4d6f71ff50
Antônio Carlos <antonioocarlos@proton.me>
2025-10-24 18:27:08 -0300

    feat (player): mute button works correctly

---
+++ src/components/util/Button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import { defineProps } from "vue";
+    const props = defineProps<{
+        action: () => void,
+    }>();
+</script>
+
+<template>
+    <button
+        class="hover:bg-fg-lighter p-1.5 rounded-md transition-[background] duration-200 flex-shrink-0"
+        @click="props.action">
+        <slot />
+    </button>
+</template>
