751613bb2e0a7b39837ee816ae02dd613c5c37a1
Antônio Carlos <antonioocarlos@proton.me>
2025-10-28 17:46:37 -0300

    feat (store): add functions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { ripple } from "./directives/ripple";
 
 import "./assets/css/index.css";
 import App from "./App.vue";
@@ -7,4 +8,5 @@ import App from "./App.vue";
 const app = createApp(App);
 
 app.use(createPinia());
+app.directive("ripple", ripple);
 app.mount("#app");
