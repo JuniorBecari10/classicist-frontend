4b00cfac5b98daad82ce324df5efd2943d3a53fc
Rodolfo Bassi Neto <rodolfobassineto@gmail.com>
2025-10-29 23:32:32 +0000

    Continuing the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 </script>
 <template>
-  <img src="src/assets/landing-page-gif.gif" class="h-50 w-full">
+  <div class="bg-[url('./src/assets/landing-page-gif.gif')] h-50 bg-cover bg-center "></div>
+  <div class="absolute left-0 top-0 w-full h-50 bg-linear-to-t from-black via-transparent to-black"></div>
+  <img src="./assets/inconeC.png" class="absolute top-3 left-3 fixed"></img>
+  <div class="absolute top-3 right-3 ">
+    <a class=" box-conten bg-transparent px-2 pt-1 pb-2 rounded-md mr-3">Login</a>
+    <a class=" box-content bg-white px-2 pt-1 pb-2 rounded-md text-black">Sign Up</a>
+  </div>
+  <div class="absolute flex justify-center grid ">
+    <h1 class="text-[40px] text-wrap w-80">The next-level classical music app.</h1>
+    <p  class="text-[24px]">Discover a new way to listen to classical music.</p>
+  </div>
   
 </template>
