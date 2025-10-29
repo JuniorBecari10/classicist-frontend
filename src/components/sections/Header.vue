9fab27302911f1a77a76c84d0e28a6353e336f10
Antônio Carlos <antonioocarlos@proton.me>
2025-10-29 15:48:05 -0300

    fix (UI): fix UI errors

---
+++ src/components/sections/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="flex items-center justify-center m-4 h-10">
+        <img class="cursor-pointer" src="@/assets/images/logo.png" />
+
+        <div class="flex-1"></div>
+
+        <div class="hover:brightness-120 flex items-center px-25 py-2 justify-center bg-fg-lighter rounded-md cursor-pointer">
+            <img class="mr-3 w-4" src="@/assets/images/search.png" />
+            <span class="text-white font-fredoka">Search</span>
+        </div>
+
+        <div class="flex-1"></div>
+
+        <div class="hover:brightness-120 flex items-center justify-center cursor-pointer">
+            <div class="bg-[#85E014] size-10 text-lg rounded-full text-white font-bold flex items-center justify-center">A</div>
+        </div>
+    </header>
+</template>
