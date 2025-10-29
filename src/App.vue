fcf7e36295ff234db03e4dae23c04b04d9dd1134
Rodolfo Bassi Neto <rodolfobassineto@gmail.com>
2025-10-29 23:46:20 +0000

    ading more text

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@
   <div class="bg-[url('./src/assets/landing-page-gif.gif')] h-50 bg-cover bg-center "></div>
   <div class="absolute left-0 top-0 w-full h-50 bg-linear-to-t from-black via-transparent to-black"></div>
   <img src="./assets/inconeC.png" class="absolute top-3 left-3 fixed"></img>
-  <div class="absolute top-3 right-3 ">
-    <a class=" box-conten bg-transparent px-2 pt-1 pb-2 rounded-md mr-3">Login</a>
-    <a class=" box-content bg-white px-2 pt-1 pb-2 rounded-md text-black">Sign Up</a>
+  <div class="absolute top-3 right-3 fixed ">
+    <a class=" box-conten bg-transparent px-2 pt-1 pb-2 rounded-md mr-3  font-[Old_Standard_TT]">Login</a>
+    <a class=" box-content bg-white px-2 pt-1 pb-2 rounded-md text-black font-[Old_Standard_TT]">Sign Up</a>
   </div>
   <div class="absolute top-50 ml-[50%] -translate-x-1/2 flex flex-col justify-start items-center">
-    <h1 class="text-[40px] text-wrap w-90 text-center">The next-level classical music app.</h1>
-    <p  class="text-[24px] text-nowrap    text-center">Discover a new way to listen to classical music.</p>
+    <h1 class="text-[40px] text-wrap w-90 text-center font-[Old_Standard_TT]">The next-level classical music app.</h1>
+    <p  class="text-[24px] text-nowrap    text-center font-[Old_Standard_TT]">Discover a new way to listen to classical music.</p>
+    <a class=" box-conten bg-white px-2 pt-1 pb-2 rounded-md mr-3 text-black  font-[Old_Standard_TT]">Listen now</a>
   </div>
   
 </template>
