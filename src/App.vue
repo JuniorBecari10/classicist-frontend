3f1511e1bdbb42dc174468c1a97ac17d62e9fc5e
Rodolfo Bassi Neto <rodolfobassineto@gmail.com>
2025-10-29 23:41:11 +0000

    centralizing the text

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@
     <a class=" box-conten bg-transparent px-2 pt-1 pb-2 rounded-md mr-3">Login</a>
     <a class=" box-content bg-white px-2 pt-1 pb-2 rounded-md text-black">Sign Up</a>
   </div>
-  <div class="absolute flex justify-center grid ">
-    <h1 class="text-[40px] text-wrap w-80">The next-level classical music app.</h1>
-    <p  class="text-[24px]">Discover a new way to listen to classical music.</p>
+  <div class="absolute top-50 ml-[50%] -translate-x-1/2 flex flex-col justify-start items-center">
+    <h1 class="text-[40px] text-wrap w-90 text-center">The next-level classical music app.</h1>
+    <p  class="text-[24px] text-nowrap    text-center">Discover a new way to listen to classical music.</p>
   </div>
   
 </template>
