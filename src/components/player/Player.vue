aa6d307ec60622cecaee01bcfa945b2d3e4ba011
Antônio Carlos <antonioocarlos@proton.me>
2025-10-29 13:00:40 -0300

    conflict (antonio): fix conflict

---
+++ src/components/player/Player.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+    import { ref } from "vue";
+    import Current from "./Current.vue";
+    import { Option } from "@/model/option.ts";
+
+    const workTitle = ref({
+        kind: "Symphony",
+        number: Option.some(5),
+        nickname: Option.none(),
+    } satisfies WorkTitle);
+
+    const displayWork = {
+        title: workTitle,
+        movement: "Allegro con brio",
+    } satisfies DisplayWork;
+
+    const performers = ref([
+        {
+            id: 0,
+            name: "Herbert von Karajan",
+        },
+    ] satisfies Performer[]);
+    
+</script>
+
+<template>
+    <div class="w-full p-5">
+        <div class="w-full h-20 bg-fg rounded-lg">
+            <Current :work="displayWork" composer="Ludwig van Beethoven" :performers="performers" />
+        </div>
+    </div>
+</template>
