bee5cc6700145fae3a62e939186cafdf41d702d9
Antônio Carlos <antonioocarlos@proton.me>
2025-10-29 13:00:40 -0300

    conflict (antonio): fix conflict

---
+++ src/model/recording.ts
@@ -0,0 +1,31 @@
+interface AudioFile {
+  path: string;
+  duration: number;
+}
+
+export interface Performer {
+  id: number;
+  name: string;
+}
+
+interface RecordingPerformer {
+  id: number;
+  performer: Performer;
+  role: string; 
+}
+
+interface RecordedMovement {
+  id: number;
+  movementIndex: number; 
+  movementId: number;
+  audioFile: AudioFile;
+}
+
+interface Recording {
+  id: number;
+  workId: number;
+  year: number;
+  photoPath: string; 
+  performers: RecordingPerformer[];
+  movements: RecordedMovement[];
+}
