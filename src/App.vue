3e701effbb7d74239f07f453b9e249f923bc3b79
Rodolfo Bassi Neto <rodolfobassineto@gmail.com>
2025-10-30 23:20:51 +0000

    Added two future clickable buttons

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,11 @@
     <a class=" box-conten bg-transparent px-2 pt-1 pb-2 rounded-md mr-3  font-[Old_Standard_TT]">Login</a>
     <a class=" box-content bg-white px-2 pt-1 pb-2 rounded-md text-black font-[Old_Standard_TT]">Sign Up</a>
   </div>
-  <div class="absolute top-50 ml-[50%] -translate-x-1/2 flex flex-col justify-start items-center">
-    <h1 class="text-[40px] text-wrap w-90 text-center font-[Old_Standard_TT]">The next-level classical music app.</h1>
+  <div class="absolute top-30 ml-[50%] -translate-x-1/2 flex flex-col justify-start items-center">
+    <h1 class="text-[40px] text-wrap w-90 text-center font-[Old_Standard_TT] leading-13 ">The next-level classical music app.</h1>
     <p  class="text-[24px] text-nowrap    text-center font-[Old_Standard_TT]">Discover a new way to listen to classical music.</p>
-    <a class=" box-conten bg-white px-2 pt-1 pb-2 rounded-md mr-3 text-black  font-[Old_Standard_TT]">Listen now</a>
+    <a  class="text-[20px] box-conten bg-white px-10 pt-3 pb-3 rounded-md text-black font-[Old_Standard_TT] mt-25">Listen now</a>
+    <a  class="text-[20px] box-conten bg-transparent px-15 pt-1 pb-2 rounded-md border-2 border-white font-[Old_Standard_TT] mt-5">See what Classicist offers</a>
   </div>
   
 </template>
