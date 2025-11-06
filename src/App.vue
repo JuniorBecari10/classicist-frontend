1cffe358c5b3345fd62ebd35251ad4d02e077134
Antônio Carlos <antonioocarlos@proton.me>
2025-11-06 11:12:18 -0300

    feat (sidebar): the library works correctly

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,6 @@
     import Center from "./components/sections/Center.vue";
     import Player from "./components/player/Player.vue";
     import ResizeSplit from "./components/util/ResizeSplit.vue";
-
-    import { usePlayerStore } from "./stores/player.ts";
-
-    const rec = {"id":9,"work_id":7,"year":1966,"photo_path":"eine.jpg","performers":[{"id":17,"performer":{"id":8,"name":"Herbert von Karajan"},"role":"Conductor"},{"id":18,"performer":{"id":9,"name":"Berliner Philharmoniker"},"role":"Orchestra"}],"movements":[{"id":17,"movement_index":0,"movement_id":17,"audio_file":{"path":"eine-1.mp3","duration":334}},{"id":18,"movement_index":0,"movement_id":18,"audio_file":{"path":"eine-2.mp3","duration":326}},{"id":19,"movement_index":0,"movement_id":19,"audio_file":{"path":"eine-3.mp3","duration":146}},{"id":20,"movement_index":0,"movement_id":20,"audio_file":{"path":"eine-4.mp3","duration":195}}]};
-
-    usePlayerStore().addRecording(rec);
 </script>
 
 <template>
