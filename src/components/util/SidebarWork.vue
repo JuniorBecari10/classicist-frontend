51000c0db5ce501bd4152b37ce441df22d830cd6
Antônio Carlos <antonioocarlos@proton.me>
2025-11-06 20:01:19 -0300

    fix (sidebarwork): fix 'x' position

--- src/components/util/SidebarWork.vue
+++ src/components/util/SidebarWork.vue
@@ -26,46 +26,46 @@
         <!-- Main clickable item -->
         <button
             class="hover:bg-fg-more-lighter transition-[background] duration-200 h-full rounded-md cursor-pointer text-left w-full p-3 ml-2
-                   flex flex-col justify-center items-start"
+                   flex"
             v-ripple
-            @click="showThis"
-        >
-            <div v-if="props.item.type === 'work'" class="w-full">
-                <p class="text-[1.15rem] font-semibold font-fredoka truncate w-[80%]">
-                    {{ formatTitleDisplay(props.item.value.title) }}
-                </p>
-                <p class="text-sm text-fgray font-fredoka">
-                    Work • {{ props.item.value.composer.name }}
-                </p>
-            </div>
+            @click="showThis">
+            <div class="flex-1 flex flex-col items-start justify-center">
+                <div v-if="props.item.type === 'work'" class="w-full">
+                    <p class="text-[1.15rem] font-semibold font-fredoka truncate w-[95%]">
+                        {{ formatTitleDisplay(props.item.value.title) }}
+                    </p>
+                    <p class="text-sm text-fgray font-fredoka">
+                        Work • {{ props.item.value.composer.name }}
+                    </p>
+                </div>
 
-            <div v-else-if="props.item.type === 'composer'" class="w-full">
-                <p class="text-[1.15rem] font-semibold font-fredoka truncate w-[80%]">
-                    {{ props.item.value.name }}
-                </p>
-                <p class="text-sm text-fgray font-fredoka">Composer</p>
-            </div>
+                <div v-else-if="props.item.type === 'composer'" class="w-full">
+                    <p class="text-[1.15rem] font-semibold font-fredoka truncate w-[95%]">
+                        {{ props.item.value.name }}
+                    </p>
+                    <p class="text-sm text-fgray font-fredoka">Composer</p>
+                </div>
 
-            <div v-else-if="props.item.type === 'performer'" class="w-full">
-                <p class="text-[1.15rem] font-semibold font-fredoka truncate w-[80%]">
-                    {{ props.item.value.name }}
-                </p>
-                <p class="text-sm text-fgray font-fredoka">Performer</p>
-            </div>
+                <div v-else-if="props.item.type === 'performer'" class="w-full">
+                    <p class="text-[1.15rem] font-semibold font-fredoka truncate w-[95%]">
+                        {{ props.item.value.name }}
+                    </p>
+                    <p class="text-sm text-fgray font-fredoka">Performer</p>
+                </div>
 
-            <div v-else>
-                what kind of thing is this? {{ props.item }}
+                <div v-else>
+                    what kind of thing is this? {{ props.item }}
+                </div>
             </div>
-        </button>
-
-        <!-- Close button, absolutely positioned -->
-        <button
-            class="absolute bottom-11 left-86 size-7 p-1.5 rounded-md hover:bg-fg-more-lighter opacity-0
-                   group-hover:opacity-100 transition-opacity duration-200"
-            v-ripple
-            @click.stop="removeThis"
-        >
-            <img src="@/assets/images/close.png" class="w-full" />
+            <div class="w-10 h-full flex justify-center items-center">
+                <button
+                    class="size-7 p-1.5 rounded-md hover:bg-fg-even-more-lighter opacity-0
+                           group-hover:opacity-100 transition-opacity duration-200 cursor-pointer"
+                    v-ripple
+                    @click.stop="removeThis">
+                    <img src="@/assets/images/close.png" class="w-full" />
+                </button>
+            </div>
         </button>
     </div>
 </template>
