552f011b65d01f6fa9023102f72efbb8b5fe1d4c
Antônio Carlos <antonioocarlos@proton.me>
2025-11-07 18:01:17 -0300

    fix (rebase): fix changes done by rebase to make the app function again

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import Composer from "./components/Composer.vue"
-import Login from "./components/Login.vue"
-import Workpage from "./components/Workpage.vue";
+    import Header from "./components/sections/Header.vue";
+    import Sidebar from "./components/sections/Sidebar.vue";
+    import Center from "./components/sections/Center.vue";
+    import Player from "./components/player/Player.vue";
+    import ResizeSplit from "./components/util/ResizeSplit.vue";
 </script>
 
 <template>
-    <!-- <Composer></Composer> -->
-    <!-- <Login></Login> -->
-    <Workpage></Workpage>
+    <div class="bg-black flex flex-col w-full min-h-screen">
+        <Header />
+
+        <div class="p-2 flex-1 flex flex-col">
+            <ResizeSplit>
+                <template #left> <Sidebar /> </template>
+                <template #right> <Center /> </template>
+            </ResizeSplit>
+
+            <Player />
+        </div>
+    </div>
 </template>
